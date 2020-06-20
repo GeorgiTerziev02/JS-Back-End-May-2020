21f918b58d88bcf6f6c44c140c7bc49a692b71d1
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2020-06-20 16:41:07 +0300

    feat: implement login

--- Cubicle/routes/auth.js
+++ Cubicle/routes/auth.js
@@ -1,11 +1,21 @@
 const { Router } = require('express');
-const { saveUser } = require('../controllers/user');
+const { saveUser, verifyUser } = require('../controllers/user');
 const router = Router();
 
 router.get('/login', (req, res) => {
     res.render('login', {
         title: 'Login  | Cube Workshop'
     });
+});
+
+router.post('/login', async (req, res) => {
+    const status = await verifyUser(req, res);
+
+    if(!status){
+        return res.redirect(301, '/login');
+    }
+
+    res.redirect('/');
 });
 
 router.get('/register', (req, res) => {
