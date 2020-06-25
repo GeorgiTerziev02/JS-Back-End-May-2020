6ddc96fcd1fcfe82bb9402ab3fdf25363b7a3ce7
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2020-06-25 13:15:49 +0300

    fix skeleton bug

--- Exam-skeleton/config/express.js
+++ Exam-skeleton/config/express.js
@@ -3,7 +3,7 @@ const handlebars = require('express-handlebars');
 const cookieParser = require('cookie-parser');
 
 module.exports = (app) => {
-    app.use(cookieParser);
+    app.use(cookieParser());
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.engine('.hbs', handlebars({
